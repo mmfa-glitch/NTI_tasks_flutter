a7bc35a1fc9bc47d2e16c804c6cd3a56f02a66d8
mmfa-glitch <amen.m1211@gmail.com>
2026-03-08 22:11:18 +0000

    Task Three Beginning Flutter

--- lib/GTasks/FeaturedProducts.dart
+++ lib/GTasks/FeaturedProducts.dart
@@ -71,37 +71,3 @@ class _PromoSectionState extends State<Featuredproducts> {
     );
   }
 }
-/*
-class Featuredproducts extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(20),
-      height: 160,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.grey[200],
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text(
-                "SNEAKERS\nOF THE WEEK",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-          ),
-
-          Expanded(
-            child: Image.asset('Assets/Images/aa.png', fit: BoxFit.cover, width: double.infinity,),
-          )
-        ],
-      ),
-    );
-  }
-}*/
